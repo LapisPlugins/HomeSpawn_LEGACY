3652fa1e529271d4749d4f072de6e7811734518d
Dart2112 <DNAblue2112@gmail.com>
2015-11-18 10:41:31 +1030

    Update to 1.2

--- src/API/Configs.java
+++ src/API/Configs.java
@@ -1,10 +1,18 @@
 package API;
 
 import net.lapismc.HomeSpawn.HomeSpawn;
+import net.lapismc.HomeSpawn.PasswordHash;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
+/**
+ * API Class to get Config Files
+ *
+ * @author Dart2112
+ */
 public class Configs {
     private HomeSpawn plugin;
 
@@ -12,38 +20,89 @@ public class Configs {
         this.plugin = plugin;
     }
 
+    /**
+     * Reloads all configs from disk
+     *
+     * @throws IOException
+     */
     public void reloadConfigs() throws IOException {
         plugin.reload("Silent");
     }
 
+    /**
+     * Returns the currently loaded Spawn.yml
+     */
     public YamlConfiguration getSpawnConfig() {
         return plugin.spawn;
     }
 
+    /**
+     * Saves the given YamlConfiguration as the Spawn.yml file
+     *
+     * @throws IOException
+     */
     public void saveSpawnConfig(YamlConfiguration SpawnConfig) throws IOException {
         plugin.spawn = SpawnConfig;
         plugin.spawn.save(plugin.spawnFile);
         plugin.reload("Silent");
     }
 
+    /**
+     * Returns the currently loaded Messages.yml
+     */
     public YamlConfiguration getMessagesConfig() {
         return plugin.messages;
     }
 
+    /**
+     * Saves the given YamlConfiguration as the Messages.yml file
+     *
+     * @throws IOException
+     */
     public void saveMessagesConfig(YamlConfiguration MessagesConfig) throws IOException {
         plugin.messages = MessagesConfig;
         plugin.messages.save(plugin.messagesFile);
         plugin.reload("Silent");
     }
 
+    /**
+     * Returns the currently loaded Passwords.yml
+     */
     public YamlConfiguration getPasswords() {
         return plugin.passwords;
     }
 
+    /**
+     * Saves the given YamlConfiguration as the Passwords.yml file
+     *
+     * @throws IOException
+     */
     public void savePasswords(YamlConfiguration Passwords) throws IOException {
         plugin.passwords = Passwords;
         plugin.passwords.save(plugin.passwordsFile);
         plugin.reload("Silent");
+    }
+
+    /**
+     * Checks if the given string matches the password on file
+     *
+     * @throws NoSuchAlgorithmException and InvalidKeySpecException
+     */
+    public boolean checkPassword(String playerName, String Password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String hash = getPasswords().getString("playerName");
+        return PasswordHash.validatePassword(Password, hash);
+    }
+
+    /**
+     * Hashes the password and saves it to the given username then saves the passwords config
+     *
+     * @throws IOException, NoSuchAlgorithmException and InvalidKeySpecException
+     */
+    public void setPassword(String playerName, String rawPassword) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String hash = PasswordHash.createHash(rawPassword);
+        YamlConfiguration passwords = getPasswords();
+        passwords.set(playerName, hash);
+        savePasswords(passwords);
     }
 
 }
--- src/API/PlayerData.java
+++ src/API/PlayerData.java
@@ -7,6 +7,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
+/**
+ * API Class to get Player Data Files
+ *
+ * @author Dart2112
+ */
 public class PlayerData {
 
     private HomeSpawn plugin;
@@ -15,23 +20,30 @@ public class PlayerData {
         this.plugin = plugin;
     }
 
+    /**
+     * Returns the currently loaded Player Data file will the given Player name
+     */
     public YamlConfiguration getHomeConfig(String PlayerName) {
         String UUID = plugin.PlayertoUUID.get(PlayerName);
         YamlConfiguration getHome = plugin.HomeConfigs.get(UUID);
         return getHome;
     }
 
-    public void saveHomesConfig(YamlConfiguration HomeConfig) {
+    /**
+     * Saves the given YamlConfiguration as the UUID in its file name
+     *
+     * @throws IOException
+     */
+    public void saveHomesConfig(YamlConfiguration HomeConfig) throws IOException {
         String name = HomeConfig.getName();
         File file = new File(plugin.getDataFolder().getAbsolutePath() + File.separator + "PlayerData" + File.separator + name);
-        try {
-            HomeConfig.save(file);
-            plugin.reload("Silent");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        HomeConfig.save(file);
+        plugin.reload("Silent");
     }
 
+    /**
+     * Returns the loaded list of Player names and UUIDs for getting a UUID from a Player name
+     */
     public HashMap<String, String> PlayerNames() {
         return plugin.PlayertoUUID;
     }
