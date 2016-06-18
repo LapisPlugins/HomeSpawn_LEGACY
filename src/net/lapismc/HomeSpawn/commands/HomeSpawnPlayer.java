2064ec187b527b631cb3b9118c227ed4e186dd1f
BuildTools <unconfigured@null.spigotmc.org>
2016-06-18 16:16:06 +0930

    Started on homespawn player command

---
+++ src/net/lapismc/HomeSpawn/commands/HomeSpawnPlayer.java
@@ -0,0 +1,18 @@
+package net.lapismc.HomeSpawn.commands;
+
+import net.lapismc.HomeSpawn.HomeSpawn;
+import org.bukkit.entity.Player;
+
+public class HomeSpawnPlayer {
+
+    private HomeSpawn plugin;
+
+    public HomeSpawnPlayer(HomeSpawn plugin) {
+        this.plugin = plugin;
+    }
+
+    public void HomeSpawnPlayer(String[] args, Player player) {
+
+    }
+
+}
